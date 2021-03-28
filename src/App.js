ddfaa67d25abdf5a1cbcce9cde6c1a86c71e8ad1
solex16 <solex16@users.noreply.github.com>
2021-03-28 18:27:22 +0100

    set up initial page layout and get API results

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import { useState, useEffect } from 'react';
+
+const API_KEY = `2f3d3a8ab38ec8199c7c0261d59c7f46`
+const API_PATH = `https://api.themoviedb.org/3/`
+const getQuery = API_PATH + `movie/now_playing?api_key=` + API_KEY
 
 function App() {
+
+  const [status, setStatus] = useState('idle');
+  const [query, setQuery] = useState(getQuery);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (!query) return; 
+
+    const fetchData = async () => {
+      setStatus('fetching');
+      const response = await fetch(query);
+      const d = await response.json();
+      setData(d.results);
+      console.log('data', d);
+      setStatus('fetched');
+    };
+
+    fetchData();
+  }, [query]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container mx-auto">
+      <header className="bg-gray-200 pl-2 h-32 border-blue-400 border-b-2">
+        <img alt='tmdb logo' src='tmdb_logo.png' />
+
       </header>
+      <div className="min-h-screen bg-gray-100">
+        Main
+
+<div className="flex flex-wrap -mx-4">
+          <div className="px-4 w-full lg:w-1/6">
+            Filter
+</div>
+          <div className="px-4 w-full lg:w-5/6">
+            <div className="grid grid-cols-3 gap-4">
+            {
+              data && data.map((d) => 
+                 <div key={d.id}>{d.title}</div>
+              )
+            }
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
